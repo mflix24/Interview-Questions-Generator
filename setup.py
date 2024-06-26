2b565941beab4307dd3459a5212562d3a497963e
mflix24 <banglacourse1223@gmail.com>
2024-06-26 11:08:42 +0600

    few changes had been done.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Question Generation Project",
+    version= '0.0.1',
+    author="Mehedi Hasan",
+    author_email="banglacourse1223@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
